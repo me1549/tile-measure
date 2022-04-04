060b2201b1cb2d157173266bfd01c3c145f08521
me1549 <yusaku49me15@gmail.com>
2022-04-04 13:05:38 +0900

    リセット修正

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
         } ?>
       </h2>
       <h2><?php echo $_GET['message'] ?></h2>
-      <a href="/tile-measure/index.php"><input type="submit" value="リセット"></a>
+      <a href="index.php"><input type="submit" value="リセット"></a>
     </div>
   </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
