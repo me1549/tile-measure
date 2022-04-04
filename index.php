be4d257b35e520acc6edb39898f52c5956575ee2
me1549 <yusaku49me15@gmail.com>
2022-04-04 13:01:48 +0900

    viewport設定

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>タイル計測</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
@@ -53,17 +54,17 @@
         </div>
       </div>
       <h2>
-      <?php for ($i = 1; $i <= 50; $i++) {
-        echo $i.'枚目'.'<br/>'.$_GET['answer'] * $i .'mm'.'<br/>';
-      } ?>
-    </h2>
-    <h2><?php echo $_GET['message'] ?></h2>
-    <a href="/tile-measure/index.php"><input type="submit" value="リセット"></a>
+        <?php for ($i = 1; $i <= 50; $i++) {
+          echo $i . '枚目' . '<br/>' . $_GET['answer'] * $i . 'mm' . '<br/>';
+        } ?>
+      </h2>
+      <h2><?php echo $_GET['message'] ?></h2>
+      <a href="/tile-measure/index.php"><input type="submit" value="リセット"></a>
     </div>
   </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 
 </html>
